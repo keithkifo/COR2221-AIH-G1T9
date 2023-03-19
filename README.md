d1737a51920aed742013026463003a9b7858c558
keithkifo <keithjigsaw@gmail.com>
2023-03-19 23:55:08 +0800

    Kickstart AIH Project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # COR2221-AIH-G1T9
+
+
+## Packages Used
+pymysql
+transitions
+google-api-client
+pyTelegramBotAPI
+python-dotenv
---
+++ db.py
@@ -0,0 +1,12 @@
+import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection = pymysql.connect(
+    host = os.getenv('DB_HOST'),
+    user = os.getenv('DB_USER'),
+    password = os.getenv('DB_PASSWORD'),
+    database = os.getenv('DB_NAME')
+)

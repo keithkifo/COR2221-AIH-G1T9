1b2ff9506d19d7098c04b9e44ccb0f4a8b38d8ad
keithkifo <keithjigsaw@gmail.com>
2023-03-20 00:41:18 +0800

    Instantiate Telegram Bot

--- db.py
+++ db.py
@@ -10,3 +10,19 @@ connection = pymysql.connect(
     password = os.getenv('DB_PASSWORD'),
     database = os.getenv('DB_NAME')
 )
+
+def check_user_status( chat_id ):
+    with connection.cursor() as cursor:
+        query = """
+            SELECT chat_id
+            FROM users
+            WHERE chat_id = '{chat_id}'
+        """.format( chat_id = chat_id )
+
+        cursor.execute( query )
+        result = cursor.fetchone()
+
+        if result == None:
+            return 'new_user'
+        
+        return 'existing_user'
